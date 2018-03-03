c472cf958f4231c8edc7823b8ff48b154d64a448
qvil <qvil1127@gmail.com>
2018-03-04 02:57:19 +0900

    Add Watermelon

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.scss';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="parent--div">
+        <div className="child--div content" id="div1" >
+          <div className="seed" />
+          <div className="seed" />
+          <div className="seed" />
+          <div className="seed" />
+          <div className="seed" />
+        </div>
+        <div className="child--div" id="div2" >
+          <div className="stripe" />
+          <div className="stripe" />
+          <div className="stripe" />
+          <div className="stripe" />
+          <div className="stripe" />
+          <div className="stripe" />
+          <div className="stripe" />
+          <div className="stripe" />
+          <div className="stripe" />
+        </div>
       </div>
     );
   }
